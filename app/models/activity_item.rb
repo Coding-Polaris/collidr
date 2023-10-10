55c59b9fa04a8d11443e4578afc67c7af75067eb
CP <seth.swenson@protonmail.com>
2023-10-10 08:28:39 -0400

    Remove annotations from specs

--- app/models/activity_item.rb
+++ app/models/activity_item.rb
@@ -1,20 +1,10 @@
-# == Schema Information
-#
-# Table name: activity_items
-#
-#  id            :bigint           not null, primary key
-#  activity_time :datetime
-#  activity_type :string           not null
-#  description   :text             not null
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  activity_id   :integer          not null
-#  user_id       :integer          not null
-#
-# Indexes
-#
-#  index_activity_items_on_activity_id  (activity_id)
-#  index_activity_items_on_user_id      (user_id)
-#
 class ActivityItem < ApplicationRecord
+  %i[
+    activity_type
+    activity_id
+    description
+    user_id
+  ].each do |field|
+    # validates field, presence: true
+  end
 end
