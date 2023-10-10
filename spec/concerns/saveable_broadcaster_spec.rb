0f460d320e3c7b975809fdec246c635dba29d819
CP <seth.swenson@protonmail.com>
2023-10-10 16:40:49 -0400

    SaveableBroadcast spec shared examples

---
+++ spec/concerns/saveable_broadcaster_spec.rb
@@ -0,0 +1,19 @@
+RSpec.shared_examples_for "SaveableBroadcaster" do
+  it "broadcasts validation failure" do
+    model.valid?
+
+    expect { model.save }.to broadcast(:save_failure, model)
+  end
+
+  it "broadcasts save failure" do
+    model.save
+
+    expect { model.save }.to broadcast(:save_failure, model)
+  end
+
+  it "broadcasts save success" do
+    model.save
+
+    expect { model.save }.to broadcast(:save_success, model)
+  end
+end
