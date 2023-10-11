1bf522fe62972dc9cb89d8fd4fa6cc774c97e104
CP <seth.swenson@protonmail.com>
2023-10-11 18:44:50 -0400

    Create Comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_173901) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_202733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_173901) do
     t.datetime "updated_at", null: false
     t.index ["activity_id"], name: "index_activity_items_on_activity_id"
     t.index ["user_id"], name: "index_activity_items_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "reply_id", null: false
+    t.string "reply_type", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["reply_id"], name: "index_comments_on_reply_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
