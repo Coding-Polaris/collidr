28c7c6cf03bc56fd98ae98231329bd1d3a59c29b
CP <seth.swenson@protonmail.com>
2023-10-11 13:58:06 -0400

    Annotate tests

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: ratings
+#
+#  id         :bigint           not null, primary key
+#  value      :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  ratee_id   :integer
+#  rater_id   :integer
+#
+# Indexes
+#
+#  index_ratings_on_ratee_id               (ratee_id)
+#  index_ratings_on_rater_id_and_ratee_id  (rater_id,ratee_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ratee_id => users.id)
+#  fk_rails_...  (rater_id => users.id)
+#
 describe Rating, type: :model do
   let(:rating) { create(:rating) }
   subject { rating }
