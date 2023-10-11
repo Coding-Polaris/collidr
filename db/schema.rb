f6fb3426922ef309e8262d1cb57bac3bd16dc55f
CP <seth.swenson@protonmail.com>
2023-10-11 13:56:22 -0400

    Foundation of Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_11_172455) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_11_173901) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_11_172455) do
     t.datetime "updated_at", null: false
     t.index ["activity_id"], name: "index_activity_items_on_activity_id"
     t.index ["user_id"], name: "index_activity_items_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "ratings", force: :cascade do |t|
